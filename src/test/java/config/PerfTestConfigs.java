f1f7d6d981ad017eeaddc622c65f85925f350711
Hemant Badaya <hemantbadaya@Hemants-MacBook-Pro-2.local>
2024-09-19 16:48:06 +0530

    Refactor chain builder

--- src/test/java/config/PerfTestConfigs.java
+++ src/test/java/config/PerfTestConfigs.java
@@ -3,7 +3,7 @@ package config;
 public class PerfTestConfigs {
 
     public static final String BASE_URL =  "https://test-api.k6.io/";
-    public static final String GET_API_RESOURCE = "/public/crocodiles/";
+    public static final String GET_API_ENDPOINT = "/public/crocodiles/";
 //    public static final double REQUEST_PER_SECOND = getAsDoubleOrElse("requestPerSecond", 10f);
 //    public static final long DURATION_MIN = getAsIntOrElse("durationMin", 1);
 //    public static final int P95_RESPONSE_TIME_MS = getAsIntOrElse("p95ResponseTimeMs", 1000);
--- src/test/java/simulation/DummyAPITest.java
+++ src/test/java/simulation/DummyAPITest.java
@@ -3,7 +3,6 @@ package simulation;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 import static config.PerfTestConfigs.*;
-import static utils.HeaderUtils.*;
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
@@ -13,17 +12,26 @@ public class DummyAPITest extends Simulation {
             .baseUrl(BASE_URL)
             .acceptHeader("application/json");
 
+
+    private static ChainBuilder getAllItems =
+            exec(http("Get First endpoint")
+                            .get(GET_API_ENDPOINT)
+                            .check(status().is(200))
+                            .check(jmesPath("[? name=='Bert'].id").saveAs("id")));
+
+    private static ChainBuilder getSpecificItem =
+            exec(http("Get specific resource")
+                    .get(GET_API_ENDPOINT + "#id/")
+                    .check(status().is(200)));
+
+
     // Define the scenario: 2 users hitting the GET endpoint
     ScenarioBuilder scenario = scenario("Test E2E flow")
-            .exec(http("Get First endpoint")
-                            .get(GET_API_RESOURCE)
-                            .check(status().is(200))
-                    .check(jmesPath("[? name=='Bert'].id").saveAs("id")))
+            .exec(getAllItems)
             .pause(1,2)
 
-            .exec(http("Get specific resource")
-                    .get(GET_API_RESOURCE + "#id/")
-                    .check(status().is(200)))
+            .repeat(2).on
+                    (exec(getSpecificItem))
             .pause(1,2);
 
     {

6ada3b082ee95a77e891e44c1b7b2f86b501212a
Hemant Badaya <hemantbadaya@Hemants-MacBook-Pro-2.local>
2024-09-19 16:28:48 +0530

    Update get api

--- src/test/java/simulation/DummyAPITest.java
+++ src/test/java/simulation/DummyAPITest.java
@@ -10,35 +10,27 @@ import io.gatling.javaapi.http.*;
 public class DummyAPITest extends Simulation {
 
     HttpProtocolBuilder httpProtocol = HttpDsl.http
-            .baseUrl("https://test-api.k6.io/")
+            .baseUrl(BASE_URL)
             .acceptHeader("application/json");
 
     // Define the scenario: 2 users hitting the GET endpoint
-    ScenarioBuilder scenario = scenario("Get request")
+    ScenarioBuilder scenario = scenario("Test E2E flow")
             .exec(http("Get First endpoint")
-                            .get("/public/crocodiles/")
+                            .get(GET_API_RESOURCE)
                             .check(status().is(200))
                     .check(jmesPath("[? name=='Bert'].id").saveAs("id")))
             .pause(1,2)
 
-            .exec(session -> {
-                int id = session.getInt("id");
-                System.out.println("/public/crocodiles/#{id}");
-                return session;
-            })
-
             .exec(http("Get specific resource")
-                    .get("/public/crocodiles/#{id}")
+                    .get(GET_API_RESOURCE + "#id/")
                     .check(status().is(200)))
-
             .pause(1,2);
-
 
     {
         // Simulate 2 users and run the scenario for 10 seconds
         setUp(
                 scenario.injectOpen(
-                        constantUsersPerSec(2).during(2) // 2 users per second for 10 seconds
+                        constantUsersPerSec(10).during(20) // 2 users per second for 10 seconds
                 )
         ).protocols(httpProtocol);
     }
